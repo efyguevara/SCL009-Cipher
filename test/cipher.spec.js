e2b879c1634f5ba9aef50903b8e1ebbc5e65ccb9
Stefany Guevara <efyguevara@gmail.com>
2019-04-05 21:20:51 -0300

    Test para comprobar funciones encode y decode 	Validacion de cifrado y descifrado 	validacion de parametros del encode y decode 	validacion de impresion de espacios en el codigo cifrado y descifrado

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -10,8 +10,22 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
-  });
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
+      assert.equal(window.cipher.encode('HOLA', 33), "OVSH");
+    });
+      
+    it('Esto deberia retornar "Por favor verifica el valor del mensaje y/o el numero de desplazamiento" para "toEncode == ""', () => {
+      assert.equal(window.cipher.encode(''), "Por favor verifica el valor del mensaje y/o el numero de desplazamiento");
+    });
+    it('debería retornar " " para " " para cualquier espacio ingresado', () => {
+      assert.equal(window.cipher.encode('HOLA MUNDO', 25), "GNKZ LTMCN");
+    });
+  })  
+/*  it('deberia ser un error', () => {
+      assert.equal(typeof cipher.encode, 'Error');
+    });*/
+   
+
 
   describe('cipher.decode', () => {
 
@@ -19,7 +33,21 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', () => {
+    assert.equal(window.cipher.decode('OVSH', 33), "HOLA");
+    });
+    it('Esto deberia retornar "Por favor verifica el valor del mensaje y/o el numero de desplazamiento" para "toDecode == "" || offSet == ""', () => {
+      assert.equal(window.cipher.decode('', 3), "Por favor verifica el valor del mensaje y/o el numero de desplazamiento");
+    });
+    it('debería retornar " " para " " para cualquier espacio ingresado', () => {
+      assert.equal(window.cipher.decode('GNKZ LTMCN', 25), "HOLA MUNDO");
+    });
   });
-
 });
+
+/*
+    it('deberia ser un error', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+    it('Esto deberia retornar un alert "Por favor verifica el valor del mensaje y/o el numero de desplazamiento" para "toDecode == "" || offSet == ""');
+  assert.equal(window.cipher.decode('a', ''), "Por favor verifica el valor del mensaje y/o el numero de desplazamiento");*/
